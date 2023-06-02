db352d9dc491b5d6d044067612d42ed8a0fc3f0e
Zillion <77107077+DevZillion@users.noreply.github.com>
2023-06-02 11:18:54 +0200

    Add KernelSU

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,14 +1,74 @@
 #!/bin/bash
 
-export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
-export CLANG_TRIPLE=aarch64-linux-gnu-
-export ARCH=arm64
+# Written by Hakalle (Velosh) <hakalle@proton.me>.
 
-export KCFLAGS=-w
-export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+# Clone GCC & Proton Clang.
+[[ -d "$(pwd)/gcc/" ]] || git clone https://github.com/VH-Devices/toolchains -b gcc-10.3.0 gcc --depth 1 >> /dev/null 2> /dev/null
+[[ -d "$(pwd)/clang/" ]] || git clone https://github.com/DevZillion/clang_r383902 -b main clang --depth 1 >> /dev/null 2> /dev/null
 
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y gta7lite_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
+# Export KBUILD_BUILD_{USER,HOST} flags.
+export KBUILD_BUILD_USER="zillion"
+export KBUILD_BUILD_HOST="github"
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+# Export ARCH/SUBARCH flags.
+export ARCH="arm64"
+export SUBARCH="arm64"
+
+# Export CCACHE
+export CCACHE_EXEC="$(which ccache)"
+export CCACHE="${CCACHE_EXEC}"
+export CCACHE_COMPRESS="1"
+export USE_CCACHE="1"
+$CCACHE -M 50G
+
+# Export toolchain/clang/llvm flags
+export CROSS_COMPILE="$(pwd)/gcc/bin/aarch64-buildroot-linux-gnu-"
+export CLANG_TRIPLE="aarch64-linux-gnu-"
+export CC="$(pwd)/clang/bin/clang"
+
+# Export if/else outdir var
+export WITH_OUTDIR=true
+
+# Clear the console
+clear
+
+# Remove out dir folder and clean the source
+if [ "${WITH_OUTDIR}" == true ]; then
+   if [ -d "$(pwd)/gta7lite" ]; then
+      rm -rf gta7lite
+   fi
+fi
+
+# Build time
+if [ "${WITH_OUTDIR}" == true ]; then
+   if [ ! -d "$(pwd)/gta7lite" ]; then
+      mkdir gta7lite
+   fi
+fi
+
+# Sync KernelSU
+if [ "${BUILD_KSU}" == true ]; then
+   # Backup stat.c
+   cp "$(pwd)/fs/stat.c" "$(pwd)/fs/stat.c.orig"
+   patch -p1 < "$(pwd)/kernelsu.patch"
+   curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
+else
+   # Build without KernelSU :)
+   # Check if exec.c.orig exists, if it exists then the KernelSU patch was previosly applied so we need to restore the original files.
+   if [ -f "$(pwd)/fs/exec.c.orig" ]; then
+      rm -rf "$(pwd)/KernelSU"
+      rm -rf "$(pwd)/fs/exec.c"
+      rm -rf "$(pwd)/fs/open.c"
+      rm -rf "$(pwd)/fs/read_write.c"
+      rm -rf "$(pwd)/fs/stat.c"
+      mv "$(pwd)/fs/exec.c.orig" "$(pwd)/fs/exec.c"
+      mv "$(pwd)/fs/open.c.orig" "$(pwd)/fs/open.c"
+      mv "$(pwd)/fs/read_write.c.orig" "$(pwd)/fs/read_write.c"
+      mv "$(pwd)/fs/stat.c.orig" "$(pwd)/fs/stat.c"
+   fi
+fi
+
+if [ "${WITH_OUTDIR}" == true ]; then
+   "${CCACHE}" make O="$(pwd)/gta7lite" gta7lite_defconfig
+   "${CCACHE}" make -j`nproc` O="$(pwd)/gta7lite"
+fi
